7f17756e9e4287e38392f2cfdc2e1b4b2e888f8b
Lucas <lucasribeiroabreu@live.com>
2025-02-11 17:05:19 -0300

    unique key

--- models/example/staging/raw_transactions.sql
+++ models/example/staging/raw_transactions.sql
@@ -1,4 +1,4 @@
-{{ config(materialized='incremental') }}
+{{ config(materialized='incremental', unique_key='id_transaction') }}
 
 SELECT *, current_localtimestamp() as loaded_at
 FROM read_csv_auto('{{ var("raw_data_path") }}')
